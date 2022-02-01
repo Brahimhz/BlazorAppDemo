2f41eb6eee13c2fd02c4f17d8b6a05d74669149d
Bouhadida Brahim Hamza <bouhadidahz95@hotmail.com>
2022-02-01 13:05:01 +0100

    Initial commit

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlazorAppDemo.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Post> Posts { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            Post[] postsToSeed = new Post[10];
+
+            for (int i = 1; i < 11; i++)
+                postsToSeed[i - 1] = new Post
+                {
+                    Id = i,
+                    Title = "Post" + i,
+                    Content = " this post number " + i
+                };
+
+            modelBuilder.Entity<Post>().HasData(postsToSeed);
+        }
+    }
+}
---
+++ Migrations/20220201115749_DataSeed.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorAppDemo.Migrations
+{
+    public partial class DataSeed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "Id", "Content", "Title" },
+                values: new object[] { 1, " this post number 1", "Post1" });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "Id", "Content", "Title" },
+                values: new object[] { 2, " this post number 2", "Post2" });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "Id", "Content", "Title" },
+                values: new object[] { 3, " this post number 3", "Post3" });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "Id", "Content", "Title" },
+                values: new object[] { 4, " this post number 4", "Post4" });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "Id", "Content", "Title" },
+                values: new object[] { 5, " this post number 5", "Post5" });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "Id", "Content", "Title" },
+                values: new object[] { 6, " this post number 6", "Post6" });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "Id", "Content", "Title" },
+                values: new object[] { 7, " this post number 7", "Post7" });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "Id", "Content", "Title" },
+                values: new object[] { 8, " this post number 8", "Post8" });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "Id", "Content", "Title" },
+                values: new object[] { 9, " this post number 9", "Post9" });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "Id", "Content", "Title" },
+                values: new object[] { 10, " this post number 10", "Post10" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Posts",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Posts",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Posts",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Posts",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Posts",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Posts",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Posts",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Posts",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Posts",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Posts",
+                keyColumn: "Id",
+                keyValue: 10);
+        }
+    }
+}
